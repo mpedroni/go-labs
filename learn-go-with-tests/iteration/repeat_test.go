791d58d693a746de2e73191bca07e53c22ac2859
Matheus Pedroni <mpedroni77@gmail.com>
2024-10-27 11:37:37 -0300

    add use example with strings.Repeat func

--- learn-go-with-tests/iteration/repeat_test.go
+++ learn-go-with-tests/iteration/repeat_test.go
@@ -2,12 +2,22 @@ package iteration
 
 import (
 	"fmt"
+	"strings"
 	"testing"
 )
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 10)
 	expected := "aaaaaaaaaa"
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
+	}
+}
+
+func TestStringsRepeat(t *testing.T) {
+	repeated := strings.Repeat("x", 7)
+	expected := "xxxxxxx"
 
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
